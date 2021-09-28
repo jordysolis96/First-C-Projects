a434dae16ee7779bbc77d6c43ecb230a5f10504c
jordy muniz <jordy.muniz96@gmail.com>
2021-09-28 10:55:06 -0500

    made my first c++ method and called it into the main method

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
+
+void name(string name)
+{
+    cout << "What is your name" << endl;
+    getline(cin, name);
+    cout << "Hello " + name + "wlecome to my first c++ app!" << endl;
+}
 
 int main()
 {
     cout << "Hello world!" << endl;
+    name("bill");
 }
